af4d02dc65fe1e087e72705405254d4c843b7cc3
vwmoose <vwmoose@users.noreply.github.com>
2023-08-14 01:18:11 +0100

    Update index.php

--- examples/index.php
+++ examples/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $faker = \Faker\Factory::create();
-$faker->addProvider(new \RauweBieten\PhpFakerClothing\Clothing($faker));
+$faker->addProvider(new \Vwmoose\PhpFakerClothing\Clothing($faker));
 
 foreach (range(1,10) as $i) {
     print $faker->clothing() . PHP_EOL;
